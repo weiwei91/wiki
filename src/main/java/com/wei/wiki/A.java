66b5e0ce36dae031ab0fbf2fd2d5defa029662c8
weiwei <weismart1@gmail.com>
2022-01-28 20:22:28 +0800

    fix ：日志优化

--- src/main/java/com/wei/wiki/A.java
+++
@@ -1,9 +0,0 @@
-package com.wei.wiki;
-
-/**
- * @Description:
- * @Auther: weiwei
- * @Date: 2022/01/28/19:57
- */
-public class A {
-}
--- src/main/java/com/wei/wiki/WikiApplication.java
+++ src/main/java/com/wei/wiki/WikiApplication.java
@@ -1,13 +1,24 @@
 package com.wei.wiki;
 
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikiApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment environment = app.run(args).getEnvironment();
+        LOGGER.info("启动成功");
+        LOGGER.info("地址：http://127.0.0.1:{}",environment.getProperty("server.port"));
     }
 
 }
