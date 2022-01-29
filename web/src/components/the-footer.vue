ddf108c02b16b9ce28259f5da7931bf5f8a3a2fa
weiwei <weismart1@gmail.com>
2022-01-29 21:47:44 +0800

    fix: fotter 固定在底部

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-layout-footer style="text-align: center">
+  <a-layout-footer style="text-align: center;position: absolute;bottom: 0; width: 100%;">
     Ant Design ©2022 Created by WeiWei
   </a-layout-footer>
 </template>
