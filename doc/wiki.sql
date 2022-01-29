8d3b94001329ce2a487af63789613e0b98c8417e
weiwei <weismart1@gmail.com>
2022-01-29 21:06:22 +0800

    fix: 修改脚本格式错误问题

--- doc/wiki.sql
+++ doc/wiki.sql
@@ -1,16 +1,20 @@
 # 电子书表
-drop table if exists 'ebook';
-create table 'ebook' (
-    'id' bigint not null comment 'id',
-    'name' varchar(50) comment '名称',
-    'description' varchar(200) comment '描述',
-    'cover' varchar(200) comment '封面',
-    'category1_id' bigint comment '分类1',
-    'category2_id' bigint comment '分类2',
-    'doc_count' int comment '文档数',
-    'view_count' int comment '阅读数',
-    'vote_count' int comment '点赞数',
-    primary key ('id')
+drop table if exists `ebook`;
+create table `ebook` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    `description` varchar(200) comment '描述',
+    `cover` varchar(200) comment '封面',
+    `category1_id` bigint comment '分类1',
+    `category2_id` bigint comment '分类2',
+    `doc_count` int comment '文档数',
+    `view_count` int comment '阅读数',
+    `vote_count` int comment '点赞数',
+    primary key (`id`)
 ) engine =innodb default charset =utf8mb4 comment ='电子书';
 
-insert into 'ebook' (id,name,description) values(1,'spring boot入门教程','零基础入门开发')
+insert into `ebook` (id,name,description) values(1,'spring boot入门教程','零基础入门开发');
+insert into `ebook` (id,name,description) values(2,'spring boot入门教程','零基础入门开发');
+insert into `ebook` (id,name,description) values(3,'spring boot入门教程','零基础入门开发');
+insert into `ebook` (id,name,description) values(4,'spring boot入门教程','零基础入门开发');
+insert into `ebook` (id,name,description) values(5,'spring boot入门教程','零基础入门开发');
