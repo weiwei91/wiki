9c44c4d226bc00c75bf67c75ec390381a949b93b
weiwei <weismart1@gmail.com>
2022-01-29 17:44:30 +0800

    fix: 解决跨域问题

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -3,7 +3,7 @@
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
           mode="inline"
-          v-model:selectedKeys="selectedKeys2"
+
           v-model:openKeys="openKeys"
           :style="{ height: '100%', borderRight: 0 }"
       >
@@ -53,5 +53,16 @@
   </a-layout>
 </template>
 <script lang="ts">
+import { defineComponent } from 'vue';
+import axios from 'axios'
 
+export default defineComponent({
+  name: 'Home',
+  setup(){
+    console.log("setup");
+    axios.get("http://localhost:81/ebook/list").then(function (response){
+        console.log(response);
+    })
+  }
+});
 </script>
