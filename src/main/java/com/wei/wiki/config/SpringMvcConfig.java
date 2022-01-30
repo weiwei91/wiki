8999cf0ffa710bd5853709048d5583e5889a5220
weiwei <weismart1@gmail.com>
2022-01-30 13:02:44 +0800

    feat: 增加拦截器打印日志

---
+++ src/main/java/com/wei/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,51 @@
+package com.wei.wiki.config;
+
+import com.wei.wiki.interceptor.ActionInterceptor;
+import com.wei.wiki.interceptor.LogInterceptor;
+import com.wei.wiki.interceptor.LoginInterceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    @Resource
+    ActionInterceptor actionInterceptor;
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**"
+                );
+
+        registry.addInterceptor(actionInterceptor)
+                .addPathPatterns(
+                        "/*/save",
+                        "/*/delete/**",
+                        "/*/reset-password");
+
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**");
+    }
+}
