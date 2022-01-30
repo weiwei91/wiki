05a3494415f798bb3d27daaf74b72cda44120490
weiwei <weismart1@gmail.com>
2022-01-30 16:02:18 +0800

    feat: 修改电子书管理页面布局

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="admin-ebook">
-    <h1>This is an admin-ebook page</h1>
-  </div>
+
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <div class="admin-ebook">
+        <h1>This is an admin-ebook page</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
