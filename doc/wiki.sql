a5c827177addc9fe23410247bc6ef6659fee42df
weiwei <weismart1@gmail.com>
2022-01-31 18:58:18 +0800

    feat: 新增快照功能

--- doc/wiki.sql
+++ doc/wiki.sql
@@ -85,16 +85,18 @@ create table `user` (
 
 insert into `user` (id,`login_name`,`name`,`password`) values (1,'test','测试','test');
 
-# 点赞表
-drop table if exists `user`;
-create table `user` (
-                        `id` bigint not null comment 'id',
-                        `login_name` varchar(50) not null comment '登陆名称',
-                        `name` varchar(50) comment '昵称',
-                        `password` char(32) not null comment '密码',
-                        primary key (`id`),
-                        unique key `login_name_unique` (`login_name`)
-) engine =innodb default charset =utf8mb4 comment ='文档内容';
+# 电子书快照表
+drop table if exists `ebook_snapshot`;
+create table `ebook_snapshot` (
+                        `id` bigint auto_increment not null comment 'id',
+                        `ebook_id` bigint not null default 0 comment '电子书id',
+                        `date` date not null comment '快照日期',
+                        `view_count` int not null default 0 comment '阅读数',
+                        `vote_count` int not null default 0 comment '点赞数',
+                        `view_increase` int not null default 0 comment '阅读增长',
+                        `vote_increase` int not null default 0 comment '点赞增长',
+                        primary key (`id`)
+) engine =innodb default charset =utf8mb4 comment ='电子书快照表';
 
 
 
--- src/main/java/com/wei/wiki/mapper/EbookSnapshotMapper.java
+++ src/main/java/com/wei/wiki/mapper/EbookSnapshotMapper.java
@@ -2,9 +2,8 @@ package com.wei.wiki.mapper;
 
 import com.wei.wiki.domain.EbookSnapshot;
 import com.wei.wiki.domain.EbookSnapshotExample;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface EbookSnapshotMapper {
     long countByExample(EbookSnapshotExample example);
