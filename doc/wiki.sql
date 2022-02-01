6ea1f55330fc65a76a961bc3ace9a2dd6137ea7b
weiwei <weismart1@gmail.com>
2022-02-01 09:52:47 +0800

    feat: 修改打包名称

--- doc/wiki.sql
+++ doc/wiki.sql
@@ -7,9 +7,9 @@ create table `ebook` (
     `cover` varchar(200) comment '封面',
     `category1_id` bigint comment '分类1',
     `category2_id` bigint comment '分类2',
-    `doc_count` int comment '文档数',
-    `view_count` int comment '阅读数',
-    `vote_count` int comment '点赞数',
+    `doc_count` int not null default 0 comment '文档数',
+    `view_count` int not null  default 0 comment '阅读数',
+    `vote_count` int not null default 0 comment '点赞数',
     primary key (`id`)
 ) engine =innodb default charset =utf8mb4 comment ='电子书';
 
